9c332333e4afad70229e0fc678f341fe2071f340
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-06-05 09:53:23 -0300

    Liskov substitution principle

---
+++ src/lsp/entities/class/order.ts
@@ -0,0 +1,32 @@
+import { ShoppingCart } from './shoppingCart';
+import { orderStatusType } from './interfaces/orderStatusType';
+import { Message } from './services/message';
+import { SaveOrder } from './services/persistence';
+
+export class Order {
+  private _orderStatus: orderStatusType = 'open';
+
+  constructor(
+    private readonly shoppingCart: ShoppingCart,
+    private readonly message: Message,
+    private readonly save: SaveOrder,
+  ) {}
+
+  get orderStatus(): orderStatusType {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.shoppingCart.isEmptyCart()) {
+      console.log('Your shopping cart is empty');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.message.sendMessage(
+      `Your request has been received, total ${this.shoppingCart.total()}`,
+    );
+    this.save.saveOrder();
+    this.shoppingCart.clearCart();
+  }
+}
